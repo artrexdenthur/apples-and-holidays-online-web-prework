a3e0343a55ae7ad64d3c1f727b28600fc85cc611
Paul Ashour <paul.ashour@gmail.com>
2018-10-09 01:45:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.map do |hash_season, hash_holiday|
     puts "#{hash_season.capitalize}:"
     hash_holiday.map do |hash_holiday, supply_array|
-      holiday_formatted = hash_holiday.to_s.split('_').each do { |x| x.capitalize }.join
+      holiday_formatted = hash_holiday.to_s.split('_').map { |x| x.capitalize }.join
       puts "  #{holiday_formatted}: #{supply_array.join(', ')}"
     end
   end
